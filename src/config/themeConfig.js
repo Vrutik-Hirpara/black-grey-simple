b5fa0e5ee322edb052a440cdbb80baa7ef5ebbe4
Vrutik Hirpara <vrutikhirpara01@gmail.com>
2026-01-20 18:13:06 +0530

    Updated project files

--- src/config/themeConfig.js
+++ src/config/themeConfig.js
@@ -1,32 +1,32 @@
 
 // black and gray
-// export const themes = {
-//   primary: "#D20000",
-//   backgroundDark: "#D20000",
-//   backgroundGray: "#7e7e7e",
-//   hover: "#ED3434",
-//   active: "#990000",
-//   backgroundBlack: "#0f0f0f",
-//   textWhite: "#ffffff",
-//     fontPrimary: `"Oxanium", Sans-serif`
-
-// };
-
-
-
-
-// blue and grey
 export const themes = {
   primary: "#D20000",
   backgroundDark: "#D20000",
   backgroundGray: "#7e7e7e",
   hover: "#ED3434",
   active: "#990000",
-  backgroundBlack: "#00001a",
+  backgroundBlack: "#0f0f0f",
   textWhite: "#ffffff",
     fontPrimary: `"Oxanium", Sans-serif`
 
 };
+
+
+
+
+// blue and grey
+// export const themes = {
+//   primary: "#D20000",
+//   backgroundDark: "#D20000",
+//   backgroundGray: "#7e7e7e",
+//   hover: "#ED3434",
+//   active: "#990000",
+//   backgroundBlack: "#00001a",
+//   textWhite: "#ffffff",
+//     fontPrimary: `"Oxanium", Sans-serif`
+
+// };
 
 
 
